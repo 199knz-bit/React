cc468ac2efc25e8cd8df6706a3f2a98651d66379
199knz-bit <199knz@unibit.bg>
2026-05-08 12:38:31 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,45 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import './css/index.css'
+import MainPage from './site_contents/mainPage.jsx'
+import Register from './login_form/register.jsx'
+import Login from './login_form/login.jsx'
+import About from './site_contents/about.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import More from './site_contents/more.jsx'
+import { FishProvider } from './contexts/FishContext.jsx'
+import { AuthProvider } from './contexts/AuthContext.jsx'
+
+const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <MainPage />,
+  },
+  {
+   path: '/register',
+   element:<Register />
+  },
+  {
+    path: '/login',
+    element:<Login />
+  },
+  {
+    path: '/more',
+    element:<More />
+  },
+  {
+    path:'/about/:fishId',
+    element:<About/>
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <FishProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </FishProvider>
+  </StrictMode>,
+)
